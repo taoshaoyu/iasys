74a7f36d2f93804ba70b40c738d153f9ac7287e4
taosy <shaoyu_tao@126.com>
2018-06-19 08:55:33 +0800

    Add filter rules

--- filter_deps.py
+++ filter_deps.py
@@ -1,8 +1,4 @@
- import re
-
-def filter_deps():
-	pass
-
+import re
 
 '''
 r:=
@@ -34,114 +30,33 @@ def get_all_conts(r):
 			ss.add(sub_item)
 	return ss
 
-
-'''
-form1:  libqb = 1.0.1-6.el7 , libcroco(armv7hl-32) = 0.6.11-1.el7
-form2:  ORBit2 >= 2.6.0  
-form3:  pkgconfig(Qt5Test)  
-'''
-
 '''
 form1:  libqb = 1.0.1-6.el7 , libcroco(armv7hl-32) = 0.6.11-1.el7
 '.+\s='
 '''
-def filter_form1(line):
+def test_filter_form1(line):
 	rule='.+\s='
 	return re.match(rule, line)
-
-def filter_not_form1(line):
-	rule='.+\s='
-	return not re.match(rule, line)
-
 
 '''
 form2:  python-idna >= 2.0  pkgconfig(gobject-2.0) >= 2.30
 '.+\s>='
 '''
-def filter_form2(line):
+def test_filter_form2(line):
 	rule='.+\s>='
 	return re.match(rule, line)
-
-def filter_not_form2(line):
-	rule='.+\s>='
-	return not re.match(rule, line)
-
 
 '''
 form3:  perl(DateTime::Locale::kpe)  libQt53DLogic.so.5(Qt_5)
-'.+(.+)'
+'.+\(.+\)'
 note: form3 is depend of all-form1-form2
 '''
-def filter_form3(line):
+def test_filter_form3(line):
 	rule='.+\(.+\)'
 	return re.match(rule, line)
 
-def filter_not_form3(line):
-	rule='.+\(.+\)'
-	return not re.match(rule, line)
 
-
-'''
-form4:  rpmlib(CompressedFileNames) <= 3.0.4-1
-'rpmlib\(.*\)'
-note: form3 is depend of all-form1-form2
-'''	
-def filter_form4(line):
-	rule='rpmlib\(.*\)'
-	return re.match(rule, line)
-
-def filter_not_form4(line):
-	rule='rpmlib\(.*\)'
-	return not re.match(rule, line)
-
-
-
-
-
-
-'''
-libFLAC++.so.6		==>  '++' will lead exception
-/usr/bin/[			==>  '['  will lead exception
-'''
-def find_all_invaild_deps():
-	ret=[]
-	conts=list(s_conts_all)[0:5]
-	for d in list(s_deps_remained):
-		for c in conts:
-			try :
-				m=re.match(d,c)
-			except:
-				ret.append(d)
-				print(d)
-				break
-	return ret
-
-'''
-libFLAC++.so.6
-dvd+rw-tools
-'''
-def find_all_plus():
-	ret=[]
-	for d in list(s_deps_remained):
-		m=re.search('\+', d)
-		if m is not None:
-			print(d)
-			ret.append(d)
-	return ret
-
-'''
-+ . * [ ] ( )
-'''	
-def find_all_meta_char():
-	ret=[]
-	for d in list(s_deps_remained):
-		m=re.search('[\.\*\+\[\]\(\)]', d)
-		if m is not None:
-			ret.append(d)
-	return ret
-
-
-def find_by_char(ch, l):
+def test_find_by_char(ch, l):
 	ret=[]
 	for d in list(l):
 		m=re.match(ch, d)
@@ -149,14 +64,139 @@ def find_by_char(ch, l):
 			ret.append(d)
 	return ret
 
-s_conts_all = get_all_conts(r)
-s_deps_all = get_all_deps(r)
-l_deps_form_1 = list( filter( filter_form1, s_deps_all ) )
-l_deps_form_2 = list( filter( filter_form2, s_deps_all ) )
-l_deps_form_3 = list( filter( filter_form3, (s_deps_all - set(l_deps_form_1) - set(l_deps_form_2)) ) )
-s_deps_remained = s_deps_all - set(l_deps_form_1) - set(l_deps_form_2) - set(l_deps_form_3)
+l_conts_all = sorted(list(get_all_conts(r)))
+l_deps_all  = sorted(list(get_all_deps(r)))
+l_deps_form_1 = list( filter( test_filter_form1, l_deps_all ) )
+l_deps_form_2 = list( filter( test_filter_form2, l_deps_all ) )
+l_deps_form_3 = list( filter( test_filter_form3, (set(l_deps_all) - set(l_deps_form_1) - set(l_deps_form_2)) ) )
 
 l_deps_char=[]
 for i in range(26):
-	l_deps_char.append(find_by_char( chr(i+ord('a') ), s_deps_all))
+	l_deps_char.append(test_find_by_char( chr(i+ord('a') ), s_deps_all))
+
+
+'''
+fm1: 
+xxx  { > < = >= <=}  yyy ==> xxx
+'''
+def filter_and_mod_1(line):
+	r=line.split()
+	if  len(r) == 3 :
+		return r[0]
+	else :
+		return line
+
+def fm1(s_deps_all):
+	return sorted(list(set(list(map(filter_and_mod_1,s_deps_all)))))
+
+'''
+xxx  { > < = >= <=}  yyy ==> xxx
+13319 -> 11713
+'''
+l_deps_all_filter_1 = fm1(l_deps_all)
+
+
+'''
+fm2: 
+xxx(armv7hl-32)  ==>  xxx
+'''
+def filter_and_mod_2(line):
+	m=re.search('\(armv7hl-32\)', line)
+	if m is None:
+		return line
+	else:
+		r=line.split('(')
+		return r[0]
+
+def fm2(l):
+	return sorted(list(set(list(map(filter_and_mod_2, l)))))
+
+l_deps_all_filter_1_2 = fm2(l_deps_all_filter_1)
+
+
+def line_has_brackets(line):
+	m=re.search('.+\(.+\)', line)
+	if m is None :
+		return False
+	else:
+		return True
+
+l_deps_all_filter_1_2_has_brackets = list(filter( line_has_brackets, l_deps_all_filter_1_2))
+l_deps_all_filter_1_2_no_brackets = list( set(l_deps_all_filter_1_2) - set(l_deps_all_filter_1_2_has_brackets))
+
+def brackets_filter_special(line):
+	m=re.match('pkgconfig\(|config\(|perl\(|font\(|mvn\(|ocaml\(|osgi\(|rpmlib\(|ruby\(|rubygem\(|php\(|tex\(|xserver-abi\(|rtld\(', line)
+	if m is not None:
+		return False
+	m=re.search('\(armv7hl-32\)', line)
+	if m is not None:
+		return False	
+	return True
+
+l_brackets_no_special = list(filter( brackets_filter_special,l_deps_all_filter_1_2_has_brackets))
+
+def brackets_get_body(l):
+	ret=[]
+	for line in l:
+		r=line.split('(')
+		ret.append(r[0])
+	return sorted(list(set(ret)))
+
+l_brackets_no_special_merged = brackets_get_body(l_brackets_no_special)   # (1) Just File
+
+
+
+def brackets_get_pkgname(l):
+	ret=[]
+	for line in l:
+		m = re.match('pkgconfig\(|config\(', line)
+		if m is None:
+			continue
+		else:
+			#print(line)
+			r=line.split('(')
+			ret.append(r[1][0:-1])
+	return ret
+
+
+l_brackets_get_packages = list(set(brackets_get_pkgname(l_deps_all_filter_1_2_has_brackets)) )   #(2) Pak name from config(xxx)  pkgconfig(xxx)
+
+
+
+'''
+Now use this function to filter all r[x][2]
+'''
+def filter_deps(deps):
+	ret_fm1 = fm1(deps)
+	ret_fm2 = fm2(ret_fm1)
+	ret_has_brackets = list(filter( line_has_brackets, ret_fm2))
+	ret_no_brackets = list( set(ret_fm2) - set(ret_has_brackets))
+	ret_has_brackets_no_special = list(filter( brackets_filter_special,ret_has_brackets))
+	ret_has_brackets_no_special_merged = brackets_get_body(ret_has_brackets_no_special)
+	ret_has_brackets_get_packages = list(set(brackets_get_pkgname(ret_has_brackets)) )
+	return set(ret_has_brackets_no_special_merged+ret_no_brackets+ret_has_brackets_get_packages)
+
+
+all_deps_filtered = filter_deps(l_deps_all)
+
+
+# Fixme: java-1.8.0-openjdk-1.8.0.161-2.b14.el7.armv7hl.rpm     mpich-3.0-3.0.4-10.el7.armv7hl.rpm
+#
+def get_pak_shot_name(long_name):
+	m=re.match('.*?-[0-9]',long_name)
+	if m is None:
+		print(long_name)
+		return None
+	else:
+		return m.group()[0:-2]
+
+def foo(l):
+	for line in l:
+		r=line.split('.')
+		sr=set(r)
+		if ('el7' not in sr) and ('el7_0' not in sr) and ('el7_1' not in sr) and ('el7_2' not in sr) and ('el7_3' not in sr) and ('el7_4' not in sr) :
+			print(line)
+
+
+#  line.split('.el7')[0]
 
--- finddep.py
+++ finddep.py
@@ -5,45 +5,42 @@ import pickle
 
 
 '''
-return {'rpmname':[f1, f2...]}
-subprocess.check_output("ls").decode('utf-8').splitlines()
-subprocess.check_output(["ls",'-l' ]).decode('utf-8').splitlines()
+[
+{"rpmname":([contents], [deps])}
+]
 '''
-def get_rpm_contents(rpmname):
-	cmd="rpm -qpl %s" %(rpmname)
-	ret=subprocess.check_output(cmd.split(' ')).decode('utf8').splitlines()
-	return {rpmname: ret}
 
-def get_rpm_deps(rpmname):
-	cmd="rpm -qpR %s" %(rpmname)
-	ret=subprocess.check_output(cmd.split(' ')).decode('utf8').splitlines()
-	return {rpmname: ret}
+def process_rpm(mirror_path, rpm_fn):
+	cmd="rpm -qpl %s" %(mirror_path+'/'+rpm_fn)
+	conts=subprocess.check_output(cmd.split(' ')).decode('utf8').splitlines()
+	cmd="rpm -qpR %s" %(mirror_path+'/'+rpm_fn)
+	deps=subprocess.check_output(cmd.split(' ')).decode('utf8').splitlines()
+	return [rpm_fn,conts, deps]
 
-def process_whole_mirror(path):
-	conts=[]
-	deps=[]
-	for f in os.listdir(path):
-		abs_fn=path+"/"+f
+def process_rpms(mirror_path):
+	ret=[]
+	for fn in os.listdir(mirror_path):
 		try:
-			conts.append(get_rpm_contents(abs_fn))
-			deps.append(get_rpm_deps(abs_fn))
+			r=process_rpm(mirror_path, fn)
+			ret.append(r)
 		except:
-			print("===error :%s ===" %(abs_fn))
-	return conts, deps
+			print("===error :%s ===" %(mirror_path+'/'+fn))
+			continue		
+	return ret
 
-def save_conts_deps(fn,conts,deps):
+def save_result(fn,ret):
 	f=open(fn,"wb")
-	pickle.dump((conts,deps), f)
+	pickle.dump(ret, f)
 	f.close()
+
+def save_conts_deps_for_whole_mirror(fn, mirror_path):
+	ret = process_rpms(mirror_path)
+	save_result(fn, ret)
 
 def load_conts_deps(fn):
 	f=open(fn,"rb")
-	c=pickle.load(f)
-	return c[0], c[1]
+	return pickle.load(f)
 
-def save_conts_deps_for_whole_mirror(fn, mirror_path):
-	conts, deps = process_whole_mirror(mirror_path)
-	save_conts_deps(fn, conts, deps)
 
 
 
